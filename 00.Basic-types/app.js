83e8d2bec3d8bd962da86f9687468427a3a47540
PRITAMALWAR <pritamkumar08102004@gmail.com>
2026-05-30 04:32:28 +0530

    today start typescript revision

---
+++ 00.Basic-types/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let UserName = "pritam";
+let age = 23;
+let isStudent = true;
+console.log(`My name is ${UserName} and Age is ${age} and am Student ${isStudent}`);
---
+++ 00.Basic-types/app.ts
@@ -0,0 +1,8 @@
+
+
+let UserName:string = "pritam";
+let age:number = 23;
+let isStudent:boolean = true;
+
+
+console.log(`My name is ${UserName} and Age is ${age} and am Student ${isStudent}`)
